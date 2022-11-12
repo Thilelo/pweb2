b4ddaf9b318899a86c81b81746f2fbfcc9eed045
lucas.sousa.gomes08@aluno.ifce.edu <asuiod35@gmail.com>
2022-11-11 21:14:25 -0300

    atualizado2

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,6 +3,13 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import {InputTextModule} from 'primeng/inputtext';
+import {ButtonModule} from 'primeng/button';
+import { FormsModule } from '@angular/forms';
+import {PasswordModule} from 'primeng/password';
+import {ImageModule} from 'primeng/image';
+
+
 
 @NgModule({
   declarations: [
@@ -10,7 +17,12 @@ import { AppComponent } from './app.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule,
+    InputTextModule,
+    ButtonModule,
+    PasswordModule,
+    ImageModule
   ],
   providers: [],
   bootstrap: [AppComponent]
